b4b0d35d81ce2ef8754a8fee100116235162e578
Gabriel Ortega <gabrielo_9103@hotmail.com>
2019-07-31 00:17:36 -0500

    index fixed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict'
 
 /* eslint no-console: "off" */
-const logger = console.log.bind(console, 'Support-massive-sqs-listener')
-
+const logger = console.log.bind(console, 'aws-node-server')
+var os = require("os");
+var hostname = os.hostname();
 const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 4044
 
 app.get('/test', (req, res) => {
-  logger(`Serving from process ${process.pid}`)
-  res.send({ process: process.pid})
+  logger(`Serving from process ${process.pid} and host ${hostname}`)
+  res.send({ process: process.pid , hostname})
 })
 
 app.listen(PORT, () => {
